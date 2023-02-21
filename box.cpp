e0802195099b01e9ae3a5ef6895f0795f745be5e
root <root@DESKTOP-4HQ71NH.localdomain>
2023-02-21 18:18:18 -0500

    updated

---
+++ box.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "box.h"
+
+std::string box(int width, int height) {
+    std::string shape;
+    for(int i=0; i<height; i++) {
+        for(int j=0; j<width; j++) {
+            shape += "*";
+        }
+        shape += "\n";
+    }
+    return shape;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "box.h"
+
+int main() {
+    std::string result;
+    result = box(3,5);
+    std::cout << "box(3,5):\n";
+    std::cout << result;
+    std::cout << "\n------------------\n";
+    
+}
